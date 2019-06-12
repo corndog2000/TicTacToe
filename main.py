c88fbd1d42cbc26989ee4cc034f0a36f90ccc913
Joe Schroedl <joe.schroedl@outlook.com>
2019-06-11 23:19:18 -0400

    Basic game done

--- main.py
+++ main.py
@@ -58,15 +58,65 @@ def userInput():
 
 
 def isGameOver():
+    # Rows
     if gameboard[0] == "O" and gameboard[1] == "O" and gameboard[2] == "O":
-        return
+        return "Player 2"
+    if gameboard[0] == "X" and gameboard[1] == "X" and gameboard[2] == "X":
+        return "Player 1"
+
+    if gameboard[3] == "O" and gameboard[4] == "O" and gameboard[5] == "O":
+        return "Player 2"
+    if gameboard[3] == "X" and gameboard[4] == "X" and gameboard[5] == "X":
+        return "Player 1"
+
+    if gameboard[6] == "O" and gameboard[7] == "O" and gameboard[8] == "O":
+        return "Player 2"
+    if gameboard[6] == "X" and gameboard[7] == "X" and gameboard[8] == "X":
+        return "Player 1"
+
+    # Columns
+    if gameboard[0] == "O" and gameboard[3] == "O" and gameboard[6] == "O":
+        return "Player 2"
+    if gameboard[0] == "X" and gameboard[3] == "X" and gameboard[6] == "X":
+        return "Player 1"
+
+    if gameboard[1] == "O" and gameboard[4] == "O" and gameboard[7] == "O":
+        return "Player 2"
+    if gameboard[1] == "X" and gameboard[4] == "X" and gameboard[7] == "X":
+        return "Player 1"
+
+    if gameboard[2] == "O" and gameboard[5] == "O" and gameboard[8] == "O":
+        return "Player 2"
+    if gameboard[2] == "X" and gameboard[5] == "X" and gameboard[8] == "X":
+        return "Player 1"
+
+    # Diagonals
+    if gameboard[0] == "O" and gameboard[4] == "O" and gameboard[8] == "O":
+        return "Player 2"
+    if gameboard[0] == "X" and gameboard[4] == "X" and gameboard[8] == "X":
+        return "Player 1"
+
+    if gameboard[2] == "O" and gameboard[4] == "O" and gameboard[6] == "O":
+        return "Player 2"
+    if gameboard[2] == "X" and gameboard[4] == "X" and gameboard[6] == "X":
+        return "Player 1"
+
+    return None
 
 
 def main():
-    while " " in gameboard:
+
+    winner = None
+    while " " in gameboard and winner == None:
         clear()
         drawGameboard()
         userInput()
+        winner = isGameOver()
+    clear()
+    drawGameboard()
+    print(f"The winner is {winner}.")
+    print(f"Player 1's moves {player1Moves}")
+    print(f"Player 2's moves {player2Moves}")
 
 
 if __name__ == '__main__':
