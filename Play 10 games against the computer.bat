b4b3e541c6f28b3e58a6c09c44738b51c6dd990a
Joe Schroedl <joe.schroedl@outlook.com>
2019-08-17 10:09:40 -0400

    Changed batch files

--- Play 10 games against the computer.bat
+++ Play 10 games against the computer.bat
@@ -1,3 +1,3 @@
-python TicTacToe.py --games 10 --count --winners --board --sample_rate 1 --save_rate 1 --user
+python TicTacToe.py --games 10 --count --winners --board --sample_rate 1 --save_rate 1 --user --model_name num1
 
 pause
--- RUN6INSTANCES.bat
+++ RUN6INSTANCES.bat
@@ -1,10 +1,10 @@
-set game_count=10000000
+set game_count=30000000
 
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num1
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num2
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num3
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num4
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num5
-start "" python TicTacToe.py --games %game_count% --count --winners --sample_rate 100000 --random --model_name num6
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num1
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num2
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num3
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num4
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num5
+start "" python TicTacToe.py --games %game_count% --count --winners --random --model_name num6
 
 pause
